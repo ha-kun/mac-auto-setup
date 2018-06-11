770ff8ed57e9786c35a01561a78d6fa5ca3c6059
ha-kun <ha-kun@users.noreply.github.com>
2018-06-11 21:04:36 +0900

    Update app.sh

--- app.sh
+++ app.sh
@@ -12,39 +12,55 @@ EOS
 # Install web apps.
 #
 echo " ----- Install web apps ------"
-brew cask install 1password
-brew cask install android-file-transfer
-brew cask install java
-brew cask install android-studio
-brew cask install arduino
-brew cask install astropad
+# brew cask install 1password
+# brew cask install android-file-transfer
+# brew cask install java
+# brew cask install android-studio
+# brew cask install arduino
+# brew cask install astropad
 brew cask install atom
 brew cask install cheatsheet
-brew cask install cyberduck
+# brew cask install cyberduck
 brew cask install docker
 brew cask install dropbox
 brew cask install duet
-brew cask install eclipse-java
-brew cask install firefox
-brew cask install genymotion
+# brew cask install eclipse-java
+# brew cask install firefox
+# brew cask install genymotion
 brew cask install google-chrome
 brew cask install iterm2
-brew cask install processing
-brew cask install skype
-brew cask install sourcetree
-brew cask install sublime-text
-brew cask install teamviewer
-brew cask install virtualbox
-brew cask install virtualbox-extension-pack
-brew cask install vlc
-brew cask install vmware-fusion
-brew cask install vmware-horizon-client
+# brew cask install processing
+# brew cask install skype
+# brew cask install sourcetree
+# brew cask install sublime-text
+# brew cask install teamviewer
+# brew cask install virtualbox
+# brew cask install virtualbox-extension-pack
+# brew cask install vlc
+# brew cask install vmware-fusion
+# brew cask install vmware-horizon-client
 brew cask install google-japanese-ime
-brew cask install swimat
-brew cask install spotify
-brew cask install imageoptim
-brew cask install hockey
-brew cask install soundflower
+# brew cask install swimat
+# brew cask install spotify
+# brew cask install imageoptim
+# brew cask install hockey
+# brew cask install soundflower
+brew cask install adobe-acrobat-pro
+
+<< NOCASKAPP
+eonkyo Downloader
+Hi-Res Audio Player
+IntelliJ IDEA
+mora downloader
+Riplt
+Scansnap
+XLD
+zipArchiver
+omnifocus
+subsonic
+qfinder
+NOCASKAPP
+
 while true; do
   read -p 'Add "need license" apps? [Y/n]' Answer
   case $Answer in
